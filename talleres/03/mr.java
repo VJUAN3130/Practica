c30b4d4a9a2f4a8bc4508c65da5c49a680e6ec9c
VJUAN3130 <juanvillavar907@gmail.com>
2026-09-22 12:41:51 -0500

    mas ejercicios

---
+++ talleres/03/mr.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class mr
+{
+    private static final int EDAD_MIN_MARATON = 18;
+    private static final int MAX_JUVENIL = 19;
+    private static final int MAX_ADULTO = 39;
+    private static final int MAX_MASTER = 49;
+
+
+    public static void main(String[] args)
+    {
+        int edad, id, nd, ke, km21, tf;
+
+    }
+}
